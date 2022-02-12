f2dc2d4202e767377a22794724ed013faad51382
Md.Sakil Mahmud <48175966+Sakil111@users.noreply.github.com>
2022-02-12 19:29:24 +0600

    Add files via upload

---
+++ list/performing multiple component in list using loop and range.py
@@ -0,0 +1,19 @@
+N = int(input("Enter a number: "))
+lst=[]
+for i in range(N):
+    cmd=input("Enter a command: ").split()
+    if cmd[0]=="insert":
+        print(cmd[0])
+        lst.insert(int(cmd[1]),int(cmd[2]))
+    elif cmd[0]=="print":
+        print(lst)
+    elif cmd[0]=="remove":
+        lst.remove(int(cmd[1]))
+    elif cmd[0]=="append":
+        lst.append(int(cmd[1]))
+    elif cmd[0]=="sort":
+        lst.sort()
+    elif cmd[0]=="pop":
+        lst.pop()
+    elif cmd[0]=="reverse":
+        lst.reverse()
